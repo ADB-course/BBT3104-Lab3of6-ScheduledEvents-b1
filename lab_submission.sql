9c55d582c07bd7aa3a8b1c56b3c70f0052d67309
Ngondo1 <167638681+Ngondo1@users.noreply.github.com>
2024-09-30 22:09:46 +0300

    creating schedule

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,2 +1,8 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
-CREATE TABLE   
+CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
+ON SCHEDULE EVERY 1 HOUR
+DO
+ SELECT 
+ AVG(TIMESTAMPDIFF(MINUTE, customer_arrival_time, customer_served_time)) 
+ AS average_waiting_time
+ FROM customer_service;
